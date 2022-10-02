07f0f2e1f10330732222ba553980eb9704d87d5b
mathiesschou <70001538+mathiesschou@users.noreply.github.com>
2022-10-02 16:23:33 +0200

    Add files via upload

--- circle.c
+++ circle.c
@@ -1,19 +1,56 @@
+/* Week 5, exercise 6 */
+#include <stdio.h>
 #include <malloc.h>
 #include "circle.h"
 
+// Point struct
+typedef struct
+{
+	int x;	// x-coordinate
+	int y;	// y-coordinate 
+} point; 
 
-void fiveCircles(circle c[]) {
-/*post: returns an array with five circles - solution to 6.b*/
+// Circle struct
+typedef struct
+{
+	point p; 	// our center-coordinate
+	int r: 		// radius 
+} circle;
+/* Postcondition: A C struct that represents a circle*/
+
+
+void fiveCircles(int c[]) 
+{
+// 	Making a for-loop to make the 5 cirlces
+	for(int i = 0; i < 5; i++)
+	{
+		c[].p.x = i;	// Our first i in (i,i)
+		c[].p.y = i;	// Our second i in (i,i)
+		c[].r = i;		// Our radius of the circle
+	}
+
+/* Postcondition: Answer to exercise 6.b */
 }
 
-int circleIsValid(const circle * c) {
-/*post: answer to exercise 6.c*/
-	return 0
+int circleIsValid(const circle * c) 
+{
+//  Checking if our radius is positive, if not; then there's no circle
+	if(c > 0)
+		return 0;
+	else if(c == 0)
+		return 1;
+	else(c < 0)
+		return 1;
+
+	return 0;
+/* Postcondition: Answer to exercise 6.c */
 }
 
-void translate(circle* c, const point* p) {
-/*ansver to exercise 6.d
+void translate(circle* c, const point* p) 
+{
+//  Adding two coordinates together
+	c -> p.x + p -> x;
+	c -> p.y + p -> y; 
+
+/* Postcondition nswer to exercise 6.d */
 }
-
-
-
--- jollyJumper.c
+++ jollyJumper.c
@@ -1,11 +1,71 @@
+#include <stdio.h>
 #include "jollyjumper.h"
 #include <stdbool.h>
-#include <malloc.h>
+//#include <malloc.h>
 
 
 
-int isJollyJumber(const int seq[], int size) {
-/*post: answer to exercise 7.a*/
+int isJollyJumber(const int seq[], int size) 
+{
+    
+    /* Creating a boolean array, where all values is false as default */
+    int exist[size - 1];
+    for(int i = 0; i < size - 1; i++)
+    {
+        exist[i] = 0;
+    }
+
+    /* For-loop with a nested if-else expression for calculating the differences */
+    for(int i = 0; i < size - 1; i++)
+    {
+        int t; 
+
+        if(seq[i] > seq[i + 1])
+        {
+            t = seq[i] - seq[i + 1];
+        }
+        else
+        {
+            t = seq[i + 1] - seq[i];
+        }
+        
+    /* If our temporary value is over 0, and t is smaller than size, return 'false'*/
+        if(t > 0 && t < size)
+            exist[t - 1] =  1;
+    }    
+    
+    /* Checking if all differences of values is present */
+        for(int i = 0; i < size; i++)
+        {
+            if(exist[i] == 0)
+            return 0;
+        }
+
+// Postcondition: answer to exercise 7.a 
     return 1;
 }
 
+int main()
+{
+    int size = 4;
+    int seq[4] = {1, 4, 2, 3};
+
+    //printf("\nInput size:\n> ");    // Getting our size
+    //scanf("%d", &size);
+
+    //printf("Input sequence:\n> ");  // Getting our sequence 
+    //for(int i = 0; i < size; i++)
+    //{
+      //scanf("%d", &seq[i]);
+    //}
+
+    /* Calling our function and using a if-else statement */
+    if(isJollyJumber(seq, size))
+    {
+        printf("Jolly");
+    }
+    else
+        printf("Not jolly");
+
+    return 0;
+}
